982d06d9a30565e083c18b51a1c669eea35d5f0d
Alex Canez <el.winy.36@gmail.com>
2024-02-05 10:07:49 -0700

    added english and spanish options

--- welcomepage.py
+++ welcomepage.py
@@ -1,34 +1,4 @@
-# Welcome to our social network project! Let's use the tools we know to perform some actions.
 
-# First, we'll display a welcome message to the user
-# and write the name of the network.
-
-# Next, we'll ask the user for some data to build their profile,
-# and we'll save this information in variables.
-
-# Finally, we'll print on the screen all the data we have collected, and allow
-# the user to write a status message.
-
-# I invite you to examine the code, read the comments, and understand the data types
-# we are using in each case.
-
-# To learn a little more about the user, we'll ask for some data.
-# For example, their year of birth, and we'll use this data to find out the user's age.
-# How will we do it? We'll use a variable to store the result of the age calculation.
-# This is an integer data type.
-
-# Next, we'll ask the user for their height in meters. This is a float data type,
-# and we'll use this information to calculate the height in centimeters.
-
-# Finally, we'll print on the screen all the data we have remembered about the user using print,
-# and we'll ask them to write a message to display on the screen.
-
-############################################################
-# The first thing we'll do is write a welcome message to the user
-# with the name of the network. You can modify this message to represent the name of your own network
-# Note that by using """ we are also delimiting a string, but by doing it this way,
-# line changes that occur in the code will be considered part of the string.
-# If you're not convinced, try changing the delimiters to "
 
 print("Welcome to ... ")
 print("""
@@ -42,80 +12,60 @@ print("""
 
 """)
 
-# First interaction. We ask the user to enter their name,
-# and we save it in a variable of type str
-name = input("To start, tell me your name. ")
+nombre = input("Whats your name? ")
 print()
-print("Hello", name, ", welcome to My Network")
+print("Hi ", nombre, ", Welcome to Fighters Fight !")
 print()
 
-# Second interaction. We ask for the entry of the year, and we use this
-# data to estimate the person's age. Why do we say we are only "estimating" their age?
-# What happens if we remove the 'int' data type conversion from the next line?
-year = int(input("To prepare your profile, tell me in which year you were born. "))
-age = 2017 - year - 1
+# Profile
+agno = int(input("Whats your birht year? "))
+edad = 2017-agno-1
 print()
 
-# Third interaction. We ask for the height, measured in meters.
-# We use the 'float' conversion and an expression to convert this
-# to a measurement in meters and centimeters.
-height = float(input("Tell me more about yourself to add it to your profile. How tall are you? Tell me in meters. "))
-height_m = int(height)
-height_cm = int((height - height_m) * 100)
 
-# Fourth interaction. We ask the user how many friends they have.
-num_friends = int(input("Very well. Finally, tell me how many friends you have. "))
+estatura = float(input("Whats your height?"))
+estatura_m = int(estatura)
+estatura_cm = int( (estatura - estatura_m)*100 )
 
-# With the collected data, we write on the screen a text that summarizes the obtained data
+
+num_amigos = int(input("How many fighters do you know? "))
+
+
 print()
-print("Very well,", name, ". So we can create a profile with this data.")
+print("Nice,", nombre, ". Lets setup ypur profile")
 print("--------------------------------------------------")
-print("Name:  ", name)
-print("Age:    ", age, "years")
-print("Height:", height_m, "meters and", height_cm, "centimeters")
-print("Friends:", num_friends)
+print("Name:  ", nombre)
+print("Age:    ", edad, "years")
+print("Height:", estatura_m, "meters", estatura_cm, "inches")
+print("Friends:  ", num_amigos)
 print("--------------------------------------------------")
-print("Thank you for the information. We hope you enjoy My Network")
+print("Thanks for making a profile, enjoy and discussed upcoming matches")
 print()
 
-#                  HEADS UP!!!
-# my code here    
-# Request gender , ph number , city , state and country , 
-gender = input("Whats your gender? ")
-print()
-print("--------------------------------------------------")
-print(name, "is a: ", gender)
+#Use bool to continue 
+continuar = True
 
+#While continue or quit
+while continuar:
+    print("What you wanna do?")
+    print("1. Write a message")
+    print("2. Change your username")
+    print("3. Quit")
 
-ph_number = input("Whats your phone number? ")
-print()
-print("--------------------------------------------------")
-print(name, "phone number is: ", ph_number)
+    opcion = input("Input the number of your selected option: ")
 
-city = input("Whats your city? ")
-print()
-print("--------------------------------------------------")
-print(name, "is at: ", city)
+    if opcion == '1':
+        mensaje = input("Write a message: ")
+        print("Message is:", mensaje)
+    elif opcion == '2':
+        nuevo_nombre = input("Choose your new username: ")
+        print("New username is:", nuevo_nombre)
+        nombre = nuevo_nombre
+    elif opcion == '3':
+        print("Bye , See you next time.")
+        continuar = False
+    else:
+        print("Option not valid , Select one of our options")
 
-state = input("Whats state is that in? ")
-print()
-print("--------------------------------------------------")
-print(name, "in the state of: ", state)
-
-country = input("Whats country is that in? ")
-print()
-print("--------------------------------------------------")
-print(name, "in the country of: ", country)
-
-
-# Finally, we request a test message that serves to publish a user's status.
-message = input("Now we are going to publish your first message. What do you think today? ")
-print()
-print("--------------------------------------------------")
-print(name, "says:", message)
-print("--------------------------------------------------")
-
-# Now you can practice by requesting more data from the user. You just have to use input() and print()
-# 1. Write 3 requests for data from the user, for example gender, phone number, city where they live,
-#    country of birth, address, etc. Save that data in variables of the appropriate type, and finally write them on the screen
-#    using print. You can add as many lines as you need.
+# exit message
+print("Thanks for using us , see you soon")
